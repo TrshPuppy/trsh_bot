e65746caaa88237ebb0f6d8c29e09be417f626b0
Rose Pineau <rosepineau@protonmail.com>
2022-12-09 13:50:30 -0500

    Cofigure node.

--- trshbot.js
+++ trshbot.js
@@ -1,8 +1,15 @@
 import * as dotenv from "dotenv";
 dotenv.config();
 
-/*
+console.log("Tiddies!");
+/*]
+NEED:
 TMI.js
+    twitch.tmi (library)
+    tmijs.com
+Twitch CLI for
+    github
+    set up authorization and test endpoints
 
 nodejs
 
@@ -27,6 +34,8 @@ TO DO:
         - nodemon (not have to restart nodee server)
 
     Node: Odin Project
+        SEDA: https://github.com/mdwelsh/mdwelsh.github.io/blob/main/papers/seda-sosp01.pdf
+
 
 
 
