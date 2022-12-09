9de639c34582ecfa4cc2437ba876b30e8ba36a26
Rose Pineau <rosepineau@protonmail.com>
2022-12-09 12:00:42 -0500

    Set up dotenv w/ node.

---
+++ trshbot.js
@@ -0,0 +1,33 @@
+import * as dotenv from "dotenv";
+dotenv.config();
+
+/*
+TMI.js
+
+nodejs
+
+TO DO:
+    OAuth token
+        Access token?
+        (havee to re-direct bot to twitch authentication page to get token)
+    OAuth Redirect URL:
+        Local host <-----
+            port: 
+        Tomcat?
+        Raspberry Pi?
+        (if other people want to uise the bot in their stream)
+    
+    Learn how to set up .env
+        install?
+        dotenv
+        Node "process.env.TOKEN" -- replace TOKEN w/ variable name
+
+    Possible dependencies:
+        - postman (to test api calls)
+        - nodemon (not have to restart nodee server)
+
+    Node: Odin Project
+
+
+
+*/
