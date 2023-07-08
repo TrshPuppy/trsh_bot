06973e77f37b7da9828e4405831a588674516808
trshpuppy <trshpuppy@proton.me>
2023-07-08 16:14:39 -0400

    flesh out new command class

---
+++ commands/CommandClass.js
@@ -0,0 +1,65 @@
+/*
+ *   Definition of a command:
+ *       - Must come from chat
+ *       - must start with the prefix set in api.json
+ */
+
+export default class Command {
+  constructor(name, args, callBack, authority) {
+    this.name = name;
+    this.args = args;
+    this.thatShitFunctionToExecute = callBack;
+    this.authority = authority;
+  }
+
+  aliases = [];
+
+  tryHandleMessage(channel, context, [arg0, arg1, arg2, ...rest]) {
+    /* Check authority first, then name, then aliases
+     *  before executing this.thatShit... (callback)
+     */
+    if (this.authority !== undefined) {
+      if (
+        this.authority.find(
+          (auth) => context.username.toLowerCase() == auth.toLowerCase()
+        ) === -1
+      ) {
+        return false;
+      }
+    }
+
+    if (this.name !== arg0) {
+      if (this.aliases.findIndex((a) => a == arg0) === -1) {
+        return false;
+      }
+    }
+
+    this.thatShitFunctionToExecute(channel, context, [arg0, arg1, ...rest]);
+
+    return true;
+  }
+
+  addManual(string, altString) {
+    if (altString) {
+      this.altManual = true;
+    }
+    this.manual = string;
+  }
+
+  getManual() {
+    if (this.altManual) {
+      return this.manual;
+    }
+    return `Command syntax: ${
+      this.manual
+    }. You can also use these aliases: ${this.aliases.map(
+      (a) => (a = ` ${a}`)
+    )}`;
+  }
+
+  addAlias(aliasArr) {
+    for (let a of aliasArr) {
+      this.aliases.push(a);
+    }
+  }
+}
