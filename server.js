da6a2fe963764c36b9ed906d4f12f6adf2d28434
trshpuppy <trshpuppy@proton.me>
2023-07-08 12:09:42 -0400

    Add some comments.

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
+/*
+ * This module is for creating and exporting the TMI.js server
+ */
+
 import apiData from "./data/api.json" assert { type: "json" };
 import tmi from "tmi.js";
 import delegateMessage from "./messages.js";
 
 export let server;
 
+// Create the server using the tmi.Client & using data in data/api.json
 export default function createServer() {
   server = new tmi.Client({
     connection: {
